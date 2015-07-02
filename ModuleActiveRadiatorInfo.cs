7b558bd51678439f4a997325ecee08cc564e9f2a
Chris Adderley <cadderley@gmail.com>
2015-07-02 12:29:28 -0700

    1.04 Changes

---
+++ ModuleActiveRadiatorInfo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace HeatControl
+{
+    public class ModuleActiveRadiatorInfo:PartModule
+    {
+
+        // Emissive Constant Extended
+        [KSPField(isPersistant = false)]
+        public float NameplateRadiationExtended = 0f;
+
+        // Emissive Constant Closed
+        [KSPField(isPersistant = false)]
+        public float NameplateRadiation = 0f;
+
+        [KSPField(isPersistant = false)]
+        public bool HasExtendedState = true;
+
+        // UI
+        // -----------
+
+        // Heat Emission UI note
+        [KSPField(isPersistant = false, guiActive = true, guiName = "Current Radiation")]
+        public string HeatRejectionGUI = "0 kW";
+
+    
+        // Radiator Status string
+        [KSPField(isPersistant = false, guiActive = true, guiName = "Radiator Temperature")]
+        public string RadiatorTemp;
+
+        public override string GetInfo()
+        {
+            string info = "";
+
+            if (!HasExtendedState)
+            {
+
+                info += String.Format("Heat Removed: {0:F1} kW", NameplateRadiation);
+            }
+            else
+            {
+
+                info += String.Format("Approx. Radiation (closed): {0:F1} kW", NameplateRadiation) + "\n" +
+                    String.Format("Approx. Radiation (deployed): {0:F1} kW", NameplateRadiationExtended);
+
+                //info += String.Format("Heat Radiated (Retracted): {0:F1} kW", HeatRadiated) + "\n" +
+                //    String.Format("Heat Radiated (Deployed): {0:F1} kW", HeatRadiatedExtended);
+            }
+
+            return info;
+
+        }
+
+
+        public override void OnFixedUpdate()
+        {
+            base.OnFixedUpdate();
+            if (HighLogic.LoadedScene == GameScenes.FLIGHT)
+            {
+                RadiatorTemp = String.Format("{0:F1} K", part.skinTemperature);
+
+                // Update the UI widget
+                HeatRejectionGUI = String.Format("{0:F1} kW", -part.thermalRadiationFlux);
+            }
+        }
+
+    }
+}
---
+++ ModuleHeatAnimator.cs
@@ -0,0 +1,68 @@
+﻿// Layered animator for heat
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace HeatControl
+{
+    public class ModuleHeatAnimator: PartModule
+    {
+
+        // Animation that plays with increasing heat
+        [KSPField(isPersistant = false)]
+        public string HeatAnimation = "";
+        // Transform for animation mixing
+        [KSPField(isPersistant = false)]
+        public string HeatTransformName = "";
+
+        private AnimationState[] heatStates;
+        private Transform heatTransform;
+
+        public override void OnStart(PartModule.StartState state)
+        {
+
+            base.OnStart(state);
+
+            // get the animation state for panel deployment
+            //deployStates = Utils.SetUpAnimation(base.animationName, part);
+
+            // Set up heat animation
+            if (HeatTransformName != "" && HeatAnimation != "")
+            {
+                heatStates = Utils.SetUpAnimation(HeatAnimation, part);
+                heatTransform = part.FindModelTransform(HeatTransformName);
+
+                foreach (AnimationState heatState in heatStates)
+                {
+                    heatState.AddMixingTransform(heatTransform);
+                    heatState.blendMode = AnimationBlendMode.Blend;
+                    heatState.layer = 15;
+                    heatState.weight = 1.0f;
+                    heatState.enabled = true;
+                }
+            }
+        }
+
+        public override void OnFixedUpdate()
+        {
+
+            base.OnFixedUpdate();
+            if (HighLogic.LoadedScene == GameScenes.FLIGHT)
+            {
+               if (HeatAnimation != "")
+                {
+                    float animFraction = (float)(part.skinTemperature / part.maxTemp);
+                    foreach (AnimationState state in heatStates)
+                    {
+                        state.normalizedTime = Mathf.MoveTowards(state.normalizedTime, Mathf.Clamp01(animFraction), 0.1f * TimeWarp.fixedDeltaTime);
+                    }
+                }
+
+            }
+        }
+
+
+    }
+}
